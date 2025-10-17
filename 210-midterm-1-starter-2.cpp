c5ac1940d35887f6fd01ac86f10bad6532580325
1080heidi <heidideleon415@gmail.com>
2025-10-16 19:31:55 -0700

    update

--- 210-midterm-1-starter-2.cpp
+++ 210-midterm-1-starter-2.cpp
@@ -59,89 +59,89 @@ public:
                 tail = newNode; 
         temp->next = newNode; // link temps next to newnode
     }
-    // delete node 
+    // delete node w a specific value
     void delete_val(int value) {
-        if (!head) return; // 
+        if (!head) return; // if list empty, nothing to delete
 
-        Node* temp = head;
+        Node* temp = head; //start from head and search
         
         while (temp && temp->data != value)
-            temp = temp->next;
+            temp = temp->next; //move forward
 
-        if (!temp) return; 
-
+        if (!temp) return;  // if value not found, exit
+        // relinnk the nodes to skip the deleted one
         if (temp->prev)
-            temp->prev->next = temp->next;
+            temp->prev->next = temp->next; // connect next nodes next to temps next
         else
-            head = temp->next; 
+            head = temp->next; //if first node is deleted move head forward
 
         if (temp->next)
-            temp->next->prev = temp->prev;
+            temp->next->prev = temp->prev; //connect next nodes prev to temps prev
         else
-            tail = temp->prev; 
+            tail = temp->prev;  // if deleting last node move tail backwards
 
-        delete temp;
+        delete temp; // free memory for removed node
     }
-
+    // delete node by position number
     void delete_pos(int pos) {
-        if (!head) {
+        if (!head) { // check if list empty
             cout << "List is empty." << endl;
             return;
         }
     
-        if (pos == 1) {
-            pop_front();
+        if (pos == 1) { // delete first node
+            pop_front(); 
             return;
         }
     
         Node* temp = head;
-    
+        // move to the node at position
         for (int i = 1; i < pos; i++){
-            if (!temp) {
-                cout << "Position doesn't exist." << endl;
+            if (!temp) { // if we run off the end of the lsit
+                cout << "Position doesn't exist." << endl; // error message
                 return;
             }
             else
                 temp = temp->next;
         }
-        if (!temp) {
+        if (!temp) { // this is a safeguard in case the pos is too big
             cout << "Position doesn't exist." << endl;
             return;
         }
-    
-        if (!temp->next) {
+     
+        if (!temp->next) { // if its the last node
             pop_back();
             return;
         }
     
-        Node* tempPrev = temp->prev;
+        Node* tempPrev = temp->prev; // connect previous and next nodes around the deleted one
         tempPrev->next = temp->next;
         temp->next->prev = tempPrev;
-        delete temp;
+        delete temp; // remove the target node from memory
     }
-
+    // add node to end of list
     void push_back(int v) {
-        Node* newNode = new Node(v);
-        if (!tail)
+        Node* newNode = new Node(v); // allocate new node
+        if (!tail) // if list empty set both head and tail
             head = tail = newNode;
-        else {
+        else { // attach after tail
             tail->next = newNode;
             newNode->prev = tail;
-            tail = newNode;
+            tail = newNode; // update tail pointer
         }
     }
-    
+    // add node to front of list
     void push_front(int v) {
-        Node* newNode = new Node(v);
-        if (!head)
-            head = tail = newNode;
-        else {
-            newNode->next = head;
-            head->prev = newNode;
-            head = newNode;
+        Node* newNode = new Node(v); /
+        if (!head) 
+            head = tail = newNode; // empty list - both head and tail pointer
+        else { 
+            newNode->next = head; // connect new node to current head
+            head->prev = newNode; // link current head back to the new node
+            head = newNode; // update head pointer
         }
     }
-    
+    // remove first node
     void pop_front() {
 
         if (!head) {
@@ -159,7 +159,7 @@ public:
             head = tail = nullptr;
         delete temp;
     }
-
+    // remove last node
     void pop_back() {
         if (!tail) {
             cout << "List is empty." << endl;
@@ -182,7 +182,8 @@ public:
             head = head->next;
             delete temp;
         }
-    }
+    } 
+    //print all ndoes from head to tail
     void print() {
         Node* current = head;
         if (!current) {
@@ -195,7 +196,7 @@ public:
         }
         cout << endl;
     }
-
+    // print all node in reverse from tail to head
     void print_reverse() {
         Node* current = tail;
         if (!current) { 
@@ -209,10 +210,13 @@ public:
         cout << endl;
     }
 };
-
+// main for testing
 int main() {
     cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS;  // dummy statement to avoid compiler warning
 
     
     return 0;
 }
+ int main(
+    cout << "not letting me commit unless i add something??"
+ ) 
